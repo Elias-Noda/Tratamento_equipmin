d810c9e6efb28d27557ac24aaee49f405841c6d0
Elias-Noda <eliassouzanoda@gmail.com>
2022-09-13 15:13:46 -0300

    Completando o tratamento

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tratamento dos relatórios de Equipe Miníma do WebSAAS
+
+Programa para auxiliar no tratamento dos dados de Equipe Miníma afim de facilitar a produção dos indicadores.
--- Tratamento.py
+++ Tratamento.py
@@ -1,10 +1,15 @@
+# To do:
+## Transformar em código baseado em funções
+## Tentar tratar xmls
+
 import pandas as pd
+import numpy as np
 
 # Colocar o arquivo no input a ser tratado aqui
 df = pd.read_csv("Input_data\R001-2014 Abr 2020.csv",
                     encoding='utf-8',
                     sep=";",
-                    decimal=".")
+                    decimal=",")
 
 # Apagando as colunas irrelevantes
 df = df.drop(["DESCRIÇÃO_CONTRATO","DESCRIÇÃO_CONTRATADA",
@@ -44,7 +49,17 @@ df["PORCENTAGEM_INTEGRAL"] = df["TOTAL_HORAS_APONTADAS"] / df["TOTAL_CARGA_HORAR
 # Criando a coluna que calcula a porcentagem sem excedente da apontada por contratada
 df["PORCENTAGEM_SEM_EXCEDENTE"] = df["TOTAL_APONTADA_SEM_EXCEDENTE"] / df["TOTAL_CARGA_HORARIA_CONTRATADA"]
 
-df.head(30)
+# Removendo os valores nulos e inf causados por divisão com 0
+df["PORCENTAGEM_INTEGRAL"].replace([np.inf, -np.inf], "Não se aplica", inplace=True)
+df["PORCENTAGEM_SEM_EXCEDENTE"] = df["PORCENTAGEM_SEM_EXCEDENTE"].fillna(0)
+
+# Colocando as colunas em ordem
+df = df.reindex(columns = ["DESCRICAO_UNIDADE", "DESCRICAO_PERIODO",
+                "TOTAL_HORAS_APONTADAS", "TOTAL_APONTADA_SEM_EXCEDENTE",
+                "TOTAL_CARGA_HORARIA_CONTRATADA", "AVALIACAO", "EXCEDENTE",
+                "PORCENTAGEM_INTEGRAL", "PORCENTAGEM_SEM_EXCEDENTE"])
+
+df.head(10)
 
 # Colocar aqui o nome que você quer para o arquivo tratado
 # df.to_csv("Output_data/R001-2014 Abr 2020.csv",
